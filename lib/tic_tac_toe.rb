e2a5d963701bcd48b92b6239e4416722f04b41c6
jane martin <223120023@student.unaki.ac.id>
2018-03-22 05:09:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ end
 def position_taken?(board,position)
   if board[position]=="X"||board[position]=="O"
     return true
-  else
+  elsif board[position]==" "||board[position]==""||board[position]==nil
     return false
   end
 end
